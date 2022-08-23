a3413cd504c6ec539b4fba188b325071dbbbcd59
Oleksandr <65237127+olek-byte@users.noreply.github.com>
2022-08-23 16:26:04 +0300

    Add files via upload

---
+++ Main-portfolio-page/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    var mixer = mixitup('.portfolio__list');
+
+    $('.zoom--js').elevateZoom({
+        zoomType: "inner",
+        cursor: "crosshair",
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750
+    });
+
+});
